8476b9ca20c524036f1948fea6db0bf300ded72a
hxlnt <partytimehexcellent@gmail.com>
2022-06-12 18:46:09 -0500

    Adds back RSS feed build steps

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
-// const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-// module.exports = function(eleventyConfig) {
-//   eleventyConfig.addPlugin(pluginRss);
-// };
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+};
